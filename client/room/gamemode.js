9ca95cb1c9a67e1e3e9895dabbf3e2909260ad43
Zttp <162319093+Zttp@users.noreply.github.com>
2025-05-08 14:39:29 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -112,6 +112,7 @@ function CheckKingDeath(killedPlayer) {
 
 // Обработчики событий
 Players.OnPlayerConnected.Add(function(p) {
+    p.Spawns.Spawn();
     p.Ui.Hint.Value = 'Добро пожаловать в битву королевств!';
 });
 
